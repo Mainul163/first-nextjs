6188dc88835d0d4ff290d9f81318556d5031f19f
mainul163 <mainulisraf163@gmail.com>
2022-09-21 12:29:52 +0600

    component add

---
+++ comp/Header.js
@@ -0,0 +1,10 @@
+
+const Header = () => {
+    return (
+        <div>
+            <p>i am header</p>
+        </div>
+    );
+};
+
+export default Header;
--- pages/home.js
+++ pages/home.js
@@ -1,6 +1,12 @@
+import Header from "../comp/header";
+
+
+
 const home = () => {
+
   return (
     <div>
+    <Header/>
       <h1>this is home page</h1>
     </div>
   );
